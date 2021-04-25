596dd3b3c92cb793b30731cace6c2e34ef6d788e
Ryan Taheri <sailantht@gmail.com>
2021-04-25 12:13:38 +0430

    6.Reference

--- src/components/player.js
+++ src/components/player.js
@@ -1,9 +1,15 @@
 //Show the song name artist and the picture
-import React from "react";
+import React, {useRef} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAngleLeft, faAngleRight, faPlay} from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({currentSong}) => {
+    //Ref. to get an element(audio)
+    const audioRef = useRef(null);
+    //Event Handlers
+    const playSongHandler = () => {
+        console.log(audioRef.current)
+    }
     return (
         <div className="player">
             <div className="time-control">
@@ -13,9 +19,10 @@ const Player = () => {
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft}/>
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay}/>
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay}/>
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight}/>
             </div>
+            <audio ref={audioRef} src={currentSong.audio}></audio>
         </div>
 
     )
